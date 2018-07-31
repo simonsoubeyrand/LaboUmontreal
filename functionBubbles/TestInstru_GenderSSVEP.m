35ccac54d4d3cf2f0d7883652604ea78d66e20e7
simonsoubeyrand <simonsoubeyrand@gmail.com>
2018-07-31 11:52:06 -0400

    First commit

---
+++ functionBubbles/TestInstru_GenderSSVEP.m
@@ -0,0 +1,11 @@
+Ceci est une tâche de reconnaissance du genre (sexe) de visages
+
+À chaque essai, vous devrez indiquer si le visage dégradé qui vous
+est présenté est celui d'un homme, ou celui d'une femme. 
+
+Vous devrez indiquer votre réponse à l''aide de touches sur le clavier:
+
+La touche «H» pour un homme.
+La touche «F» pour une femme.
+
+Vous pourrez faire une pause après chaque bloc.
